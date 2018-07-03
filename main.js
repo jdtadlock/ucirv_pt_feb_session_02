e1a7b22c8ab5a061bc34f948864796479e73eefe
JD <jt7903@gmail.com>
2018-07-03 10:04:40 -0400

    initial

---
+++ main.js
@@ -0,0 +1,39 @@
+// $(".title").css("color", "blue");
+
+
+// $(".title").text("Something Else");
+
+
+// $(".menu a:nth-child(2)").css("background", "orange");
+
+
+$(".fa-bars").click(function () {
+
+
+
+
+  // $("#logo").text("somethingelse");
+
+  $(".menu").css("display", "flex");
+
+
+
+});
+
+
+$(".fa-times-circle").click(function () {
+
+  // Hide the menu div
+  $(".menu").hide(); // sets the element's css to display: none;
+
+});
+
+
+
+
+
+
+
+
+
+
